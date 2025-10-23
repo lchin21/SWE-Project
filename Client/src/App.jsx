2a44b9847e6432a76e26631b66dc639faed33b78
ebaucom21 <elijah@elijahbau.com>
2025-10-23 10:54:39 -0400

    Initial implementation of login page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,23 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme.js";
+import { AuthenticationForm } from "./AuthenticationForm.js";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return (
+    <MantineProvider theme={theme}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100vw",
+          backgroundColor: "#404040ff"
+        }}
+      >
+        <AuthenticationForm />
+      </div>
+    </MantineProvider>
+  );
 }
